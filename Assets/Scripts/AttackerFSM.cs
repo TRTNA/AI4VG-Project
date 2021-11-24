9b635eab455be823c0108016b8ff869f617ef23e
Andrea <andreataroni1@gmail.com>
2021-11-24 16:56:46 +0100

    Modified AttackerFSM.cs to take health system and new gate behaviour into account
    Attacking behaviour of attacker has been implemented through a coroutine which applies damage to the target (through its HealthController) at a fixed rate.
    Breaching behaviour now takes into account the Gate component, which can be asked about the state of the gate (open/closed)

--- Assets/Scripts/AttackerFSM.cs
+++ Assets/Scripts/AttackerFSM.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +9,20 @@ using UnityEngine.AI;
 [RequireComponent(typeof(Animator))]
 public class AttackerFSM : MonoBehaviour
 {
-    public Transform[] fortsGates = new Transform[4];
+    public GameObject[] fortsGates = new GameObject[4];
     [Range(1, 10)]
-    public int reactionTime;
+    public int reactionTime = 1;
 
     public float walkSpeed = 3.0f;
     public float runSpeed = 7.0f;
+    public float attackCooldown = 10.0f;
 
     public float interactionRay = 2.0f; //cambiare nome
     private FSM fsm;
     private Transform nearestGate;
     private GameObject target;
+
+    private bool attackingCoroutineRunning = false;
 
     private Animation anim;
 
@@ -26,9 +30,10 @@ public class AttackerFSM : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animation>();
+        anim.playAutomatically = false;
         for (int i = 0; i < 4; i++)
         {
-            fortsGates[i] = GameObject.Find("Fort/Gate" + i).transform;
+            fortsGates[i] = GameObject.Find("Fort/Gate" + i);
         }
 
         FSMState approaching = new FSMState();
@@ -40,15 +45,16 @@ public class AttackerFSM : MonoBehaviour
         breaching.stayActions.Add(Attack);
 
         FSMState searching = new FSMState();
+        searching.enterActions.Add(FreeTarget);
         searching.stayActions.Add(Search);
 
-        FSMTransition approachingToBreachingTransition = new FSMTransition(NearestGateIsCloseAndActive);
+        FSMTransition approachingToBreachingTransition = new FSMTransition(NearestGateIsCloseToCharacterAndCanBeBreached);
         approaching.AddTransition(approachingToBreachingTransition, breaching);
 
-        FSMTransition breachingToSearchingTransition = new FSMTransition(NearestGateIsCloseAndBreached);
+        FSMTransition breachingToSearchingTransition = new FSMTransition(NearestGateIsBreachedAndIsCloseToCharacter);
         breaching.AddTransition(breachingToSearchingTransition, searching);
 
-        FSMTransition approachingToSearchingTransition = new FSMTransition(NearestGateIsCloseAndBreached);
+        FSMTransition approachingToSearchingTransition = new FSMTransition(NearestGateIsBreachedAndIsCloseToCharacter);
         approaching.AddTransition(approachingToSearchingTransition, searching);
 
         FSMState attacking = new FSMState();
@@ -57,20 +63,29 @@ public class AttackerFSM : MonoBehaviour
         FSMState chasing = new FSMState();
         chasing.stayActions.Add(Chase);
 
-        FSMTransition searchingToAttackingTransition = new FSMTransition(TargetIsSetAndClose);
+        FSMTransition searchingToAttackingTransition = new FSMTransition(TargetIsClose);
         searching.AddTransition(searchingToAttackingTransition, attacking);
 
-        FSMTransition searchingToChasingTransition = new FSMTransition(TargetIsSetButNotClose);
+        FSMTransition searchingToChasingTransition = new FSMTransition(TargetIsFar);
         searching.AddTransition(searchingToChasingTransition, chasing);
 
-        FSMTransition chasingToAttackingTransition = new FSMTransition(TargetIsSetAndClose);
+        FSMTransition chasingToAttackingTransition = new FSMTransition(TargetIsClose);
         chasing.AddTransition(chasingToAttackingTransition, attacking);
 
-        FSMTransition attackingToChaseTransition = new FSMTransition(TargetIsSetButNotClose);
+        FSMTransition attackingToChaseTransition = new FSMTransition(TargetIsFar);
         attacking.AddTransition(attackingToChaseTransition, chasing);
+
+        FSMTransition attackingToSearchingTransition = new FSMTransition(TargetIsUnavailable);
+        attacking.AddTransition(attackingToSearchingTransition, searching);
+        
 
         fsm = new FSM(approaching);
         StartCoroutine(UpdateFSM());
+    }
+
+    private bool TargetIsUnavailable()
+    {
+        return target == null;
     }
 
     // Update is called once per frame
@@ -79,24 +94,24 @@ public class AttackerFSM : MonoBehaviour
         
     }
 
-    private bool TargetIsSetAndClose()
+    private bool TargetIsClose()
     {
         return target != null && Vector3.Distance(target.transform.position, gameObject.transform.position) < interactionRay;
     }
 
-    private bool TargetIsSetButNotClose()
+    private bool TargetIsFar()
     {
         return target != null && Vector3.Distance(target.transform.position, gameObject.transform.position) > interactionRay;
     }
 
-    private bool NearestGateIsCloseAndActive()
+    private bool NearestGateIsCloseToCharacterAndCanBeBreached()
     {
-        return nearestGate.gameObject.activeInHierarchy && Vector3.Distance(gameObject.transform.position, nearestGate.position) < interactionRay;
+        return ! nearestGate.gameObject.GetComponent<Gate>().IsOpen() && Vector3.Distance(gameObject.transform.position, nearestGate.position) < interactionRay;
     }
 
-    private bool NearestGateIsCloseAndBreached()
+    private bool NearestGateIsBreachedAndIsCloseToCharacter()
     {
-        return ! nearestGate.gameObject.activeInHierarchy && Vector3.Distance(gameObject.transform.position, nearestGate.position) < interactionRay;
+        return nearestGate.gameObject.GetComponent<Gate>().IsOpen() && Vector3.Distance(gameObject.transform.position, nearestGate.position) < interactionRay;
     }
 
     private void Search()
@@ -108,16 +123,17 @@ public class AttackerFSM : MonoBehaviour
 
     private void Breach()
     {
-        //implementare breaching
-        target = nearestGate.gameObject;
         Debug.Log(gameObject.name + " is breaching a gate!");
     }
 
     private void Attack()
     {
-        //implementare attacco a target
-        anim.Play("Attack1");
-        Debug.Log(gameObject.name + " is attacking!");
+        if (target != null && ! attackingCoroutineRunning)
+        {
+            Debug.Log("Starting attacking coroutine");
+            attackingCoroutineRunning = true;
+            StartCoroutine(AttackTarget());
+        }
     }
 
     private void Chase()
@@ -128,6 +144,11 @@ public class AttackerFSM : MonoBehaviour
             GetComponent<NavMeshAgent>().speed = runSpeed;
             gameObject.GetComponent<NavMeshAgent>().destination = target.transform.position;
         }
+    }
+
+    private void FreeTarget()
+    {
+        target = null;
     }
 
 
@@ -144,16 +165,19 @@ public class AttackerFSM : MonoBehaviour
     private void FindNearestGate()
     {
         nearestGate = GetNearestGatePosition();
+        target = nearestGate.gameObject;
     }
 
     private Transform GetNearestGatePosition()
     {
         ArrayList distances = new ArrayList();
-        foreach (Transform gate in fortsGates)
+        foreach (GameObject gateObject in fortsGates)
         {
-            distances.Add((gate.position - gameObject.transform.position).sqrMagnitude);
+            Gate gate = gateObject.GetComponent<Gate>();
+            distances.Add((gateObject.transform.position - gameObject.transform.position).sqrMagnitude);
+            
         }
-        return fortsGates[distances.IndexOf(distances.ToArray().Min())];
+        return fortsGates[distances.IndexOf(distances.ToArray().Min())].transform;
     }
 
     IEnumerator UpdateFSM()
@@ -163,5 +187,26 @@ public class AttackerFSM : MonoBehaviour
             fsm.Update();
             yield return new WaitForSeconds(reactionTime);
         }
+    }
+
+    IEnumerator AttackTarget()
+    {
+        while(true && target != null)
+        {
+            if (target != null && target.gameObject.GetComponent<HealthController>().GetHealth() == 0)
+            {
+                target = null;
+                attackingCoroutineRunning = false;
+                anim.Play("Idle");
+                yield break;
+            }
+            anim.Play("Attack1");
+            Debug.Log(gameObject.name + " is attacking!");
+            target.SendMessage("TakeDamage", 10);
+            yield return new WaitForSeconds(attackCooldown);
+
+        }
+        anim.Play("Idle");
+        attackingCoroutineRunning = false;
     }
 }
