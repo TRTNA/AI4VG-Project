4465ac4e8937ac9b07c5ce72f6545f293a58e430
Andrea <andreataroni1@gmail.com>
2022-01-10 15:03:46 +0100

    Added HFSM implementation

--- Assets/Scripts/FSM.cs
+++ Assets/Scripts/FSM.cs
@@ -97,3 +97,127 @@ public class FSM {
 		}
 	}
 }
+
+public interface IFSMState
+{
+    public void AddTransition(FSMTransition transition, IFSMState target);
+
+    public FSMTransition VerifyTransitions();
+
+    public IFSMState NextState(FSMTransition t);
+
+    // These methods will perform the actions in each list
+    public void Enter();
+    public void Stay();
+    public void Exit();
+
+    public void Update();
+}
+
+public class HFSMState : IFSMState
+{
+
+    // Arrays of actions to perform based on transitions fire (or not)
+    // Getters and setters are preferable, but we want to keep the source clean
+    public List<FSMAction> enterActions = new List<FSMAction>();
+    public List<FSMAction> stayActions = new List<FSMAction>();
+    public List<FSMAction> exitActions = new List<FSMAction>();
+
+    // A dictionary of transitions and the states they are leading to
+    private Dictionary<FSMTransition, IFSMState> links;
+
+    public HFSMState()
+    {
+        links = new Dictionary<FSMTransition, IFSMState>();
+    }
+
+    public void AddTransition(FSMTransition transition, IFSMState target)
+    {
+        links[transition] = target;
+    }
+
+    public FSMTransition VerifyTransitions()
+    {
+        foreach (FSMTransition t in links.Keys)
+        {
+            if (t.myCondition()) return t;
+        }
+        return null;
+    }
+
+    public IFSMState NextState(FSMTransition t)
+    {
+        return links[t];
+    }
+
+    // These methods will perform the actions in each list
+    public virtual void Enter() { foreach (FSMAction a in enterActions) a(); }
+    public void Stay() { foreach (FSMAction a in stayActions) a(); }
+    public void Exit() { foreach (FSMAction a in exitActions) a(); }
+
+    public void Update()
+    {
+		//nothing to do i'm a state
+    }
+
+
+}
+
+public class SubMachineState : IFSMState
+{
+    // Current state
+    public IFSMState current;
+    public HFSMState me;
+
+    public SubMachineState(IFSMState state)
+    {
+        me = new HFSMState();
+        current = state;
+        current.Enter();
+    }
+
+    public void AddTransition(FSMTransition transition, IFSMState target)
+    {
+        ((IFSMState)me).AddTransition(transition, target);
+    }
+
+    public void Enter()
+    {
+        me.Enter();
+    }
+
+    public void Exit()
+    {
+        me.Exit();
+    }
+
+    public IFSMState NextState(FSMTransition t)
+    {
+        return me.NextState(t);
+    }
+
+    public void Stay()
+    {
+        me.Stay();
+    }
+
+    public void Update()
+    {
+        FSMTransition transition = current.VerifyTransitions();
+		if (transition != null) {
+			current.Exit();		// 1
+			transition.Fire();	// 2
+			current = current.NextState(transition);	// 3
+			current.Enter();	// 4
+		} else
+        {
+            current.Update();
+			current.Stay();		// 5
+		}
+    }
+
+    public FSMTransition VerifyTransitions()
+    {
+        return me.VerifyTransitions();
+    }
+}
